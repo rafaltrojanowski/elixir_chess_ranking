45738d8272d3eb00ff9bb1160e04af6ae21fff5f
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-07-25 21:42:13 +0700

    Add custom mix task

---
+++ lib/mix/tasks/sac_sac_mate.import_players.ex
@@ -0,0 +1,19 @@
+defmodule Mix.Tasks.SacSacMate.ImportPlayers do
+  use Mix.Task
+  require Logger
+
+  alias SacSacMate.Repo
+  alias SacSacMate.Accounts.Player
+  alias SacSacMate.Services.PlayerImporter
+
+  def run(_) do
+    Mix.Task.run "app.start"
+
+    PlayerImporter.call()
+    players_imported_count = length(Repo.all(Player))
+
+    Logger.info """
+    Imported players: #{inspect players_imported_count}
+    """
+  end
+end
