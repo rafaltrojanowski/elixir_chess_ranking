40b9e27f3b5422a9f2b28bddbd1e957502672320
Rafał Trojanowski <rt.trojanowski@gmail.com>
2019-08-05 16:23:33 +0700

    Heroku set up

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :sac_sac_mate, SacSacMateWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "secret-waters-61649", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
